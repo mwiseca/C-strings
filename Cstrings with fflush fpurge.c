b7421d7eef077b2c40f689787d5ff7324c8ca759
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2023-09-12 09:20:36 -0400

    Update Cstrings with fflush fpurge.c

--- Cstrings with fflush fpurge.c
+++ Cstrings with fflush fpurge.c
@@ -128,17 +128,20 @@ int main(){
        char mem [2000];
        char value [2000];
  while(1){
- printf("Enter a name of varable m for main.\n");
+ printf("//Enter a name of varable m for main.\n");
+ printf("//");
  fgets(name,2000,stdin);
  name[strcspn(name,"\n")]=0;
  if(strcmp(name,"m")==0){
  break;}
- printf("Enter the amount of memory you want to alocate.\n");
- printf("Add one for the null character.\n");
- printf("Or press enter to not alocate.\n");
+ printf("//Enter the amount of memory you want to alocate.\n");
+ printf("//Add one for the null character.\n");
+ printf("//Or press enter to not alocate.\n");
+ printf("//");
  fgets(mem,2000,stdin);
  mem[strcspn(mem,"\n")]=0;
  printf("enter a value\n");
+ printf("//");
  fgets(value,2000,stdin);
  value[strcspn(value,"\n")]=0;
  printf("%s%s%s%s%s%s%s%s%s%s\n",v[0],name,v[1],mem,v[2],v[3],v[4],value,v[4],v[5]);
