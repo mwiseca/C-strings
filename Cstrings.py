b1a0cf6d787dee41860392f7d27c161919404447
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2023-11-04 00:21:24 -0400

    Add files via upload

--- Cstrings.py
+++ Cstrings.py
@@ -40,10 +40,14 @@ eng = {
     "jj": 'scanf("%',
     "kk": '[^\\n]",',
     "ll": ");",
-    "mm": "getchar();"
+    "mm": "getchar();",
+    "nn": "char",
+    "oo": " = (char*)malloc",
+    "pp": " *",
+    "qq": "free("
 }
 def choice():
-    print("//Enter a for strings h to hide user input with getpass.")
+    print("//Enter a for strings hd to hide user input with getpass.")
     print("//Enter aa for arrays.")
     print("//Enter b for strings with new line character.")
     print("//Enter c for strings with no quotation marks.")
@@ -52,6 +56,7 @@ def choice():
     print("//Enter fs for fgets with strlen function to remove new line character.")
     print("//Enter fc for void functions.");
     print("//Enter s for scanf for strings.")
+    print("//Enter h to allocate memory from the heap.")
     print("//Enter cf to call functions.")
     print("//Enter i for if statement for strings.")
     print("//Enter e for  if else statement.")
@@ -80,7 +85,7 @@ while True:
             if text == "m":
                 break
             print(eng["a"] + text + eng["l"] + eng["b"])
-    elif switch == "h":
+    elif switch == "hd":
         import getpass
         print("//Enter text m for main.")
         while True:
@@ -208,6 +213,18 @@ while True:
             print("//Enter values for parameters if any.")
             per = input("//")
             print(name + eng["dd"] + per + eng["gg"])
+    elif switch == "h":
+        while True:
+            print("//Enter the name of the variable m for main.")
+            name = input("//")
+            if name == "m":
+                break
+            print("//Enter memory to allocate.")
+            mem = input("//")
+            print("\n")
+            print(eng["nn"] + eng["pp"] + name + eng["u"] + "\n")
+            print(name + eng["oo"] +eng["dd"] + mem + eng["w"] + "\n")
+            print(eng["qq"] + name + eng["w"] + "\n") 
     elif switch == "cy":
         while True:
             print("#include <stdio.h>")
@@ -215,6 +232,8 @@ while True:
             print("#include <stdio_ext.h>")
             print("\n")
             print("#include <string.h>")
+            print("\n")
+            print("#include <stdlib.h>")
             print("\n")
             print("while (1){")
             print("\n")
