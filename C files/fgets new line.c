e9b638ab94eb1540d602d026c3d34e45e735ce14
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2023-12-12 10:03:27 -0500

    Add files via upload

--- C files/fgets new line.c
+++ C files/fgets new line.c
@@ -4,22 +4,41 @@
   
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main (){
      char f  [6] [15] = {"fgets(", ",", "stdin);", "[strcspn(","\"\\n", "\")]=0;"};
-     char v [50];
-     int me;
+     char *v;
+     char *me;
 	
  while (1){
+ v = (char*)malloc(2000* sizeof(char));
+ me = (char*)malloc(2000* sizeof(char));
+ if(v == NULL){
+ printf("Cant allocate memory.\n");
+ exit(1);}
+ if(me == NULL){
+ printf("No memory.\n");
+ exit(1);}
  printf("//Enter name of variable x to exit.\n");
  printf("//"); 
- scanf("%49s",v);
+ fgets(v,2000,stdin);
+ v[strcspn(v,"\n")]=0;
  if (strcmp(v,"x")==0){
+ free(v);
+ v = NULL;
+ free(me);
+ me = NULL;
  break;}
  printf("//Enter the memory you alocated to the variable.\n");
  printf("//");
- scanf("%10d",&me);
- printf("%s%s%s%d%s%s\n",f[0], v, f[1],me,f[1],f[2]);
+ fgets(me,2000,stdin);
+ me[strcspn(me,"\n")]=0;
+ printf("%s%s%s%s%s%s\n",f[0], v, f[1],me,f[1],f[2]);
  printf("%s%s%s%s%s%s\n", v, f[3], v, f[1], f[4] ,f[5]);
+ free(v);
+ v = NULL;
+ free(me);
+ me = NULL;
  }
  return 0;
 }
