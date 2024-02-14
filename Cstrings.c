0c503b97bcc14997ed0471a4634f1ef62a26a240
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-02-14 09:52:28 -0500

    Update Cstrings.c

--- Cstrings.c
+++ Cstrings.c
@@ -19,7 +19,7 @@ void choice(){
  printf("//Enter s for scanf for strings to allow for white spaces.\n");
  printf("//Enter sb for scanf basic no white spaces.\n");
  printf("//Enter si for scanf for integers and doubles.\n");
- printf("//Enter sh for scanf for ints and doubles from the heap.\n");
+ printf("//Enter sh for scanf for ints and doubles on the heap.\n");
  printf("//Enter h to allocate memory on the heap.\n");
  printf("//Enter hi to allocate int double or float memory on the heap.\n");
  printf("//Enter cm to check malloc.\n");
