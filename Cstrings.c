53780d8018ba781a9873b4656636d2b5dbc0c8e7
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-09-01 08:34:24 -0400

    Update Cstrings.c

--- Cstrings.c
+++ Cstrings.c
@@ -102,7 +102,7 @@ int main(){
  printf("%s%s%s%s%s%s\n\n", v, f[3], v, f[1], f[4] ,f[5]);
      }
 }else if (strcmp(sw,"fs")==0){
-     char f  [6] [15] = {"fgets(", ",", "stdin);", "[strlen(",")-1] = '\\0';"};
+     char f  [5] [15] = {"fgets(", ",", "stdin);", "[strlen(",")-1] = '\\0';"};
      char v [2000];
      char me [2000];
 	
@@ -118,7 +118,7 @@ int main(){
  fgets(me,2000,stdin);
  me[strcspn(me,"\n")]=0;
  printf("\n%s%s%s%s%s%s\n",f[0], v, f[1],me,f[1],f[2]);
- printf("%s%s%s%s%s\n\n", v, f[3], v, f[4] ,f[5]);
+ printf("%s%s%s%s\n\n", v, f[3], v, f[4]);
  }
 }else if (strcmp(sw,"s")==0){
          char t [6] [15] = {"scanf(\"","%","[^\\n]\"",",",");","getchar();"};
