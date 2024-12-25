dc83a23d263934dd3be5eebcb4f968fe4a11ff8a
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-12-25 10:30:14 -0500

    Update Cstrings-repeat.c

--- Cstrings-repeat.c
+++ Cstrings-repeat.c
@@ -876,7 +876,7 @@ int main() {
         } else if (strcmp(sw, "ch") == 0) {
             choice();
         } else {
-            printf("//Enter a letter in Choices.\n\n");
+            printf("//Enter a letter in choices.\n\n");
         }
     }
     return 0;
