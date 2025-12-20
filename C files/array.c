9b10793aa597759787df2ec0f4faee4d47644cfc
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2025-12-20 10:19:33 -0500

    Refactor input loop for better readability

--- C files/array.c
+++ C files/array.c
@@ -1,36 +1,35 @@
 //Copyright 2023 Mitchell E Wise 
 //SPDX-License-Identifier: Apache-20 
 
-
 #include <stdio.h>
 #include <string.h>
 int main(){
-	char t [5] [15] = {"char "," [","] ", "] = {","};"};
+    char t [5] [15] = {"char "," [","] ", "] = {","};"};
 	char name [2000];
 	char ne [2000];
 	char m [2000];
 	char elements [2000];
- while(1){
- printf("//Enter the name of array x to exit.\n");
- printf("//");
- fgets(name,2000,stdin);
- name[strcspn(name,"\n")]=0;
- if(strcmp(name,"x")==0){
- break;}
- printf("//Enter the amount of elements.\n");
- printf("//");
- fgets(ne,2000,stdin);
- ne[strcspn(ne,"\n")]=0;
- printf("//Enter memory allocated to elements.\n");
- printf("//");
- fgets(m,2000,stdin);
- m[strcspn(m,"\n")]=0;
- printf("//Enter elements with quotations and commas between each.\n");
- printf("//");
- fgets(elements,2000,stdin);
- elements[strcspn(elements,"\n")]=0;
- printf("%s%s%s%s%s%s%s%s%s%s\n",t[0],name,t[1],ne,t[2],t[1],m,t[3],elements,t[4]);
- }
- return 0;
+    while(1){
+        printf("//Enter the name of array x to exit.\n");
+        printf("//");
+        fgets(name,2000,stdin);
+        name[strcspn(name,"\n")]=0;
+        if(strcmp(name,"x")==0){
+            break;
+		}
+        printf("//Enter the amount of elements.\n");
+        printf("//");
+        fgets(ne,2000,stdin);
+        ne[strcspn(ne,"\n")]=0;
+        printf("//Enter memory allocated to elements.\n");
+        printf("//");
+        fgets(m,2000,stdin);
+        m[strcspn(m,"\n")]=0;
+        printf("//Enter elements with quotations and commas between each.\n");
+        printf("//");
+        fgets(elements,2000,stdin);
+        elements[strcspn(elements,"\n")]=0;
+        printf("%s%s%s%s%s%s%s%s%s%s\n",t[0],name,t[1],ne,t[2],t[1],m,t[3],elements,t[4]);
+   }
+return 0;
 }
-
