79cc445bc0cf4e7b1caf27b03f6ef713101e98b3
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-04-29 09:17:08 -0400

    Update copyright year from 2025 to 2026

--- Cstrings.c
+++ Cstrings.c
@@ -1,4 +1,4 @@
-// Copyright 2023-2025 Mitchell E Wise
+// Copyright 2023-2026 Mitchell E Wise
 // SPDX-License-Identifier: Apache-20
 
 #include <stdio.h>
@@ -2467,7 +2467,7 @@ void copy() {
 int main() {
     char sw[SIZE];
 
-    printf("\n          copyright 2025 Mitchell E Wise\n");
+    printf("\n          copyright 2023-2026 Mitchell E Wise\n");
     printf("          SPDX-License-Identifier: Apache-20\n\n\n");
 
     printf("//Enter r to repeat choices enter to not.\n");
