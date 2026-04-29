a50c579e2e4d686a25f0de4591471dd02a77ee2e
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-04-29 09:28:36 -0400

    Refactor checkFgets for memory allocation and input
    Refactor memory allocation and input handling in checkFgets function. Update error messages and improve clarity.

--- Malloc/CstringsMallocOut.c
+++ Malloc/CstringsMallocOut.c
@@ -266,91 +266,110 @@ void fgetsSizeof() {
 }
 
 void checkFgets() {
-    const char *f[12] = {"while(fgets(", ",", "stdin) == NULL) {", "[strcspn(", "\"\\n", "\")]=0;", "    ","sizeof(","),stdin) == NULL) {","printf(\"","\\n","\");"};
+    char f[14][25] = {"while(1) {","if(fgets(","while(fgets(", ",", "stdin) == NULL) {", "[strcspn(", "\"\\n", "\")]=0;", "    ","sizeof(","),stdin) == NULL) {","printf(\"","\\n","\");"};
     char *v;
     char me [10] = "\0";
     char *em;
     char *function;
     int m;
-
-    v = (char *)malloc(SIZE * sizeof(char));
-    em = (char *)malloc(SIZE * sizeof(char));
-    function = (char *)malloc(SIZE * sizeof(char));
-    if (v == NULL || em == NULL || function == NULL) {
-        printf("Error: Insufficient memory.\n");
+    v = (char*)malloc(SIZE* sizeof(char));
+    em = (char*)malloc(SIZE* sizeof(char));
+    function = (char*)malloc(SIZE* sizeof(char));
+    if(v == NULL || em == NULL || function == NULL) {
+        printf("\nInsufficient memory.\n\n");
         exit(1);
-    }
+    } 
     while (1) {
         printf("//Enter name of variable m for main.\n");
         printf("//");
         while(fgets(v, SIZE, stdin) == NULL) {
-            checkInput();   
-        } 
+            checkInput();
+        }
         v[strcspn(v, "\n")] = 0;
-        if(strlen(v) >= MAX){
-            clean();
+        if(strlen(v) >= 9){
+            clear();
         }
         if (strcmp(v, "m") == 0) {
             break;
         }
         printf("//Enter the memory to be allocated to the variable 5 minimum.\n");
         printf("//");
-        while(fgets(me, sizeof(me), stdin) == NULL || (1)){
-            clearerr(stdin);  
+        while(fgets(me, sizeof(me), stdin) == NULL || (1)) {
+            clearerr(stdin);
             if(strlen(me) >= 9){
-                clean();
-            }            
+                clear();
+            }
             m = atoi(me);
             if(m <=4) {
                 printf("\n//Enter a number 5 or more.\n\n");
             } else {
                 break;
             }
-        } 
+        }
         me[strcspn(me, "\n")] = 0;
         printf("//Enter a error message. Invalid input is good.\n");
         while(fgets(em,SIZE,stdin) == NULL) {
-            checkInput();     
+            checkInput();
         }
         em[strcspn(em,"\n")]=0;
         if(strlen(em) >= MAX){
-            clean();
+            clear();
         }
-        printf("//Enter the function to clear input buffer name.\n");
+        printf("//Enter the name of the function to clear input buffer.\n");
         while(fgets(function,SIZE,stdin) == NULL) {
-            checkInput();     
+            checkInput();
         }
         function[strcspn(function,"\n")]=0;
         if(strlen(function) >= MAX){
-            clean();
+            clear();
         }
-        printf("\n%s%s%s%s%s%s\n",f[0], v, f[1], me, f[1], f[2]);
-        printf("%sclearerr(stdin);\n",f[6]);
-        printf("%s%s%s%s%s%s%s\n",f[6],f[9],f[10],em,f[10],f[10],f[11]); 
+        printf("\n%s%s%s%s%s%s\n",f[2], v, f[3], me, f[3], f[4]);
+        printf("%sclearerr(stdin);\n",f[8]);
+        printf("%s%s%s%s%s%s%s\n",f[8],f[11],f[12],em,f[12],f[12],f[13]);
         printf("}\n");
-        printf("%s%s%s%s%s%s\n", v, f[3], v, f[1], f[4], f[5]);
+        printf("%s%s%s%s%s%s\n", v, f[5], v, f[3], f[6], f[7]);
         printf("if(strlen(%s) >= %d) {\n",v,m - 1);
         printf("    %s();\n",function);
         printf("}\n\n");
-        printf("\n%s%s%s%s%s%s\n",f[0], v, f[1],f[7],v, f[8]);
-        printf("%sclearerr(stdin);\n",f[6]);
-        printf("%s%s%s%s%s%s%s\n",f[6],f[9],f[10],em,f[10],f[10],f[11]);
+        printf("\n%s%s%s%s%s%s\n",f[2], v, f[3],f[9],v, f[10]);
+        printf("%sclearerr(stdin);\n",f[8]);
+        printf("%s%s%s%s%s%s%s\n",f[8],f[11],f[12],em,f[12],f[12],f[13]);
         printf("}\n");
-        printf("%s%s%s%s%s%s\n", v, f[3], v, f[1], f[4], f[5]);
+        printf("%s%s%s%s%s%s\n", v, f[5], v, f[3], f[6], f[7]);
         printf("if(strlen(%s) >= %d) {\n",v,m - 1);
         printf("    %s();\n",function);
         printf("}\n\n");
-        printf("\n%s%s%s%s%s%s\n",f[0], v, f[1], me, f[1], f[2]);
-        printf("%sclearerr(stdin);\n",f[6]);
-        printf("%s%s%s%s%s%s%s\n",f[6],f[9],f[10],em,f[10],f[10],f[11]); 
+        printf("\n%s\n",f[0]); 
+        printf("%s%s%s%s%s%s%s\n",f[8],f[1], v, f[3], me, f[3], f[4]);
+        printf("%s%sclearerr(stdin);\n",f[8],f[8]);
+        printf("%s%s%s%s%s%s%s%s\n",f[8],f[8],f[11],f[12],em,f[12],f[12],f[13]);
+        printf("%s%scontinue;\n",f[8],f[8]); 
+        printf("%s}\n",f[8]);
+        printf("%s%s%s%s%s%s%s\n",f[8], v, f[5], v, f[3], f[6], f[7]);
+        printf("    if(strlen(%s) >= %d) {\n",v,m - 1);
+        printf("        %s();\n",function);
+        printf("%s}\n\n",f[8]);
+        printf("\n%s\n",f[0]); 
+        printf("%s%s%s%s%s%s%s\n",f[8],f[1], v, f[3],f[9],v, f[10]);
+        printf("%s%sclearerr(stdin);\n",f[8],f[8]);
+        printf("%s%s%s%s%s%s%s%s\n",f[8],f[8],f[11],f[12],em,f[12],f[12],f[13]);
+        printf("%s%scontinue;\n",f[8],f[8]);
+        printf("%s}\n",f[8]);
+        printf("%s%s%s%s%s%s%s\n",f[8], v, f[5], v, f[3], f[6], f[7]);
+        printf("    if(strlen(%s) >= %d) {\n",v,m - 1);
+        printf("        %s();\n",function);
+        printf("    }\n\n");
+        printf("\n%s%s%s%s%s%s\n",f[2], v, f[3], me, f[3], f[4]);
+        printf("%sclearerr(stdin);\n",f[8]);
+        printf("%s%s%s%s%s%s%s\n",f[8],f[11],f[12],em,f[12],f[12],f[13]);
         printf("}\n");
-        printf("%s%s%s%s%s%s\n", v, f[3], v, f[1], f[4], f[5]);
+        printf("%s%s%s%s%s%s\n", v, f[5], v, f[3], f[6], f[7]);
         printf("\n\n");
-        printf("\n%s%s%s%s%s%s\n",f[0], v, f[1],f[7],v, f[8]);
-        printf("%sclearerr(stdin);\n",f[6]);
-        printf("%s%s%s%s%s%s%s\n",f[6],f[9],f[10],em,f[10],f[10],f[11]);
+        printf("\n%s%s%s%s%s%s\n",f[2], v, f[3],f[9],v, f[10]);
+        printf("%sclearerr(stdin);\n",f[8]);
+        printf("%s%s%s%s%s%s%s\n",f[8],f[11],f[12],em,f[12],f[12],f[13]);
         printf("}\n");
-        printf("%s%s%s%s%s%s\n", v, f[3], v, f[1], f[4], f[5]);
+        printf("%s%s%s%s%s%s\n", v, f[5], v, f[3], f[6], f[7]);
         printf("\n\n");
         memset(me, '\0', sizeof(me));
         if (strcmp(repeat, "r") != 0) {
@@ -362,7 +381,7 @@ void checkFgets() {
     free(em);
     em = NULL;
     free(function);
-    function = NULL; 
+    function = NULL;
 }
 
 void stringToInt() { 
