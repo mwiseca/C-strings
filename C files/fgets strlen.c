bcc421deddbe4091b9821664b53ab65cbc8009ef
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-05-17 09:10:08 -0400

    Refactor while loop and formatting in fgets example

--- C files/fgets strlen.c
+++ C files/fgets strlen.c
@@ -5,25 +5,26 @@
 #include <stdio.h>
 #include <string.h>
 int main (){
-     char f  [5] [15] = {"fgets(", ",", "stdin);", "[strlen(",")-1] = '\\0';"};
-     char v [2000];
-     char me [2000];
+    char f  [5] [15] = {"fgets(", ",", "stdin);", "[strlen(",")-1] = '\\0';"};
+    char v [2000];
+    char me [2000];
 	
- while (1){
- printf("//Enter name of variable x to exit.\n");
- printf("//"); 
- fgets(v,2000,stdin);;
- v[strcspn(v,"\n")]=0;
- if (strcmp(v,"x")==0){
- break;}
- printf("//Enter the memory you allocated to the variable.\n");
- printf("//");
- fgets(me,2000,stdin);
- me[strcspn(me,"\n")]=0;
- printf("%s%s%s%s%s%s\n",f[0], v, f[1],me,f[1],f[2]);
- printf("%s%s%s%s\n", v, f[3], v, f[4]);
- }
- return 0;
+    while (1){
+        printf("//Enter name of variable x to exit.\n");
+        printf("//"); 
+        fgets(v,2000,stdin);;
+        v[strcspn(v,"\n")]=0;
+        if (strcmp(v,"x")==0){
+            break;
+	    }
+        printf("//Enter the memory you allocated to the variable.\n");
+        printf("//");
+        fgets(me,2000,stdin);
+        me[strcspn(me,"\n")]=0;
+        printf("%s%s%s%s%s%s\n",f[0], v, f[1],me,f[1],f[2]);
+        printf("%s%s%s%s\n", v, f[3], v, f[4]);
+   }
+return 0;
 }
 
  
