b9829e5f52501c4e4fe9cd57f78fa01f1af44ca0
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-07-02 22:28:05 -0400

    Improve input handling in array function
    Refactor input handling for array elements and improve clarity in user prompts.

--- Function pointer/CstringsArrays.c
+++ Function pointer/CstringsArrays.c
@@ -1640,55 +1640,64 @@ void variableValue() {
 
 void array() {
     char t[5][15] = {"char ", " [", "] ", "] = {", "};"};
+    char elements[10][150];
+    int count = 0; 
     char name[SIZE];
-    char ne[SIZE];
-    char m[SIZE];
-    char elements[SIZE];
-    while (1) {
-        printf("//Enter the name of array m for main.\n");
-        printf("//");
-        while(fgets(name, SIZE, stdin) == NULL){
+    char ne[SIZE] ;
+    char m[SIZE] ;
+    printf("//Enter the name of array m for main.\n");
+    printf("//");
+    while(fgets(name, SIZE, stdin) == NULL){
+        checkInput();
+    }
+    name[strcspn(name, "\n")] = 0;
+    if (strlen(name) >= MAX) {
+        clear();
+    }
+    if (strcmp(name, "m") == 0) {
+        return;
+    }
+    printf("//Enter the amount of elements.\n");
+    printf("//");
+    while(fgets(ne, SIZE, stdin) == NULL) {
+        checkInput();
+    }
+    ne[strcspn(ne, "\n")] = 0;
+    if (strlen(ne) >= MAX) {
+        clear();
+    }
+    printf("//Enter memory allocated to elements.\n");
+    printf("//");
+    while(fgets(m, SIZE, stdin) == NULL) {
+        checkInput();
+    }
+    m[strcspn(m, "\n")] = 0;
+    if (strlen(m) >= MAX) {
+        clear();
+    }
+    while (count < 10) {
+        printf("//Enter a element press # to stop.\n"); 
+        while(fgets(elements[count],150, stdin) == NULL) { 
             checkInput();
         }
-        name[strcspn(name, "\n")] = 0;
-        if (strlen(name) >= MAX) {
-            clear(); 
-        } 
-        if (strcmp(name, "m") == 0) {
+        elements[count][strcspn(elements[count], "\n")] = 0;
+        if (strlen(elements[count]) > 148) {
+            clear();
+        }
+        if(strcmp(elements[count],"#")==0) {
             break;
-        }
-        printf("//Enter the amount of elements.\n");
-        printf("//");
-        while(fgets(ne, SIZE, stdin) == NULL) {
-            checkInput();
-        }
-        ne[strcspn(ne, "\n")] = 0;
-        if (strlen(ne) >= MAX) {
-            clear(); 
-        } 
-        printf("//Enter memory allocated to elements.\n");
-        printf("//");
-        while(fgets(m, SIZE, stdin) == NULL) {
-            checkInput();
-        }
-        m[strcspn(m, "\n")] = 0;
-        if (strlen(m) >= MAX) {
-            clear(); 
-        } 
-        printf("//Enter elements with quotations and commas between each.\n");
-        printf("//");
-        while(fgets(elements,SIZE, stdin) == NULL) {
-            checkInput();
-        }
-        elements[strcspn(elements, "\n")] = 0;
-        if (strlen(elements) >= MAX) {
-            clear(); 
-        } 
-        printf("\n%s%s%s%s%s%s%s%s%s%s\n\n", t[0], name, t[1], ne, t[2], t[1], m, t[3], elements, t[4]);
-        if (strcmp(repeat, "r") != 0) {
-            break;
+        }else{
+            count++;
         }
     }
+    printf("\n%s%s%s%s%s%s%s%s", t[0], name, t[1], ne, t[2], t[1], m, t[3]);
+    for (int i = 0; i < count; i++) {
+        printf("%s%s%s","\"",elements[i],"\"");
+        if(i < count -1) {
+            printf(","); 
+        } 
+    }
+    printf("%s\n\n",t[4]); 
 }
 
 void numArray() {
